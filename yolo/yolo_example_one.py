63e81a5a42ccfb64136463d5ed52ab828645ab78
gokceKy <gaia.gkce@gmail.com>
2024-10-25 23:27:26 +0300

    Add odev_uc

--- yolo/yolo_example_one.py
+++ yolo/yolo_example_one.py
@@ -4,8 +4,11 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Resmi tahmin et
-results = model.predict(r"C:\Users\Gokce\Documents\bilgisayar_görmesi\yolo\people.jpg")
+source="https://user-images.githubusercontent.com/54944384/78804250-7fb77f00-79f2-11ea-9fa1-8c7253eed09d.png"
 
-# Sonuçları göster
+# Resmi tahmin et
+# results = model.predict(r"C:\Users\Gokce\Documents\bilgisayar_görmesi\yolo\people.jpg")
+
+results = model(source)
 for result in results:
     result.show()
